3e5c41b482097b0a2f624cf9e61330d43d201144
Daria Morgun <daryamorgun@gmail.com>
2015-06-13 14:55:13 +0300

    Commit

--- 7. Fibonacci number.js
+++ 7. Fibonacci number.js
@@ -6,18 +6,69 @@
 //
 //Define a function called 'fibonacci' with a single argument n. The function should return the nth number of the fibonacci sequence.
 
-function fibonacci (n) {
-    var sequence = [0,1];
-    for (var i = 1; i < n; i++) {
-        var l = sequence.length;
-        var F = sequence[l-1] + sequence[l-2];
-        var total = sequence.push(F);
-        //console.log(sequence);
-        var L = sequence.length;
-        //var Fib = console.log(sequence[L-1]);
-    }
-    return console.log(sequence[L-1]);
+//function fibonacci (n) {
+//    var sequence = [0,1];
+//    for (var i = 1; i < n; i++) {
+//        var l = sequence.length;
+//        var F = sequence[l-1] + sequence[l-2];
+//        var total = sequence.push(F);
+//        //console.log(sequence);
+//        var L = sequence.length;
+//        //var Fib = console.log(sequence[L-1]);
+//    }
+//    return console.log(sequence[L-1]);
+//
+//}
 
+
+
+function fibonacci1(n) {
+    var result;
+    if (n === 0 || n === 1) {
+        result =  n;
+    } else {
+        result = fibonacci(n-1) + fibonacci(n-2);
+    }
+    return result;
 }
 
-fibonacci(1);
+function fibonacci(n) {
+    var result,
+        prev = 1,
+        prevOfPrev = 0;
+
+
+    if (n === 0 ){
+        result = prevOfPrev;
+    } else if (n === 1){
+        result = prev;
+    } else {
+
+        var i;
+        console.log('Fibonacci =  ', n);
+        //console.log('Number of iterations ', n - i +1);
+        for (i = 2; i <= n; i++) {  //N-i,  N-1 = 2
+            result = prev + prevOfPrev;
+
+            prevOfPrev = prev;
+            prev = result;
+        }
+
+    }
+
+    return result;
+}
+
+
+console.log('Result of 0 is ',fibonacci(0),'should be ', 0, '\n');
+console.log('Result of 1 is ',fibonacci(1),'should be ', 1, '\n');
+console.log('Result of 2 is ',fibonacci(2),'should be ', 1, '\n');
+console.log('Result of 3 is ',fibonacci(3),'should be ', 2, '\n');
+console.log('Result of 4 is ',fibonacci(4),'should be ', 3, '\n');
+console.log('Result of 10 is ',fibonacci(10),'should be ', 55, '\n');
+console.log('Result of 50 is ',fibonacci(40),'should be ', 12586269025, '\n');
+
+/* 1. mocha + chai - use for #2 below
+2. Write tests for Artem implementation
+3. Write your own implementation of fibonacci
+ */
